d68cd1dcf69e5dd856f17bcd7c9791b741276fba
Malvin Sutanto <malvinsutanto@gmail.com>
2021-06-01 18:31:11 +0900

    Adds tests for TabLayout

---
+++ sample/src/main/kotlin/io/github/kakaocup/sample/TabLayoutActivity.kt
@@ -0,0 +1,19 @@
+package io.github.kakaocup.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+
+class TabLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_layout)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val tabs = listOf("Tab1", "Tab2")
+        tabs.forEach { tab ->
+            tabLayout.addTab(tabLayout.newTab().setText(tab))
+        }
+    }
+}
