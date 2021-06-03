71c998c9091087542547f75b8a5a62ea5d9f82a2
Konstantin Aksenov <aksenov.kostya@gmail.com>
2021-06-03 01:41:16 -0700

    Ignore isPlatformPopup line

--- sample/src/androidTest/kotlin/io/github/kakaocup/sample/AutoCompleteTest.kt
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/AutoCompleteTest.kt
@@ -24,7 +24,7 @@ class AutoCompleteTest {
 
             list {
                 inRoot {
-                    isPlatformPopup()
+                    //isPlatformPopup()
                 }
 
                 isVisible()
