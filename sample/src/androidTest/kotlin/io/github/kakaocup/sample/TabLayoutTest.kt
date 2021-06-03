6d1f91dfe42963df9b7b5b1d50ea01c100d93b9c
Malvin Sutanto <malvinsutanto@gmail.com>
2021-06-03 09:12:11 +0900

    Fixes ViewAction for TabLayoutActions.getSelectedItem (#22)
    * Fixes ViewAction for TabLayoutActions.getSelectedItem
    
    * Renames id to position
    
    * Adds tests for TabLayout

---
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/TabLayoutTest.kt
@@ -0,0 +1,32 @@
+package io.github.kakaocup.sample
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import io.github.kakaocup.kakao.screen.Screen.Companion.onScreen
+import io.github.kakaocup.sample.screen.TabLayoutActivityScreen
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class TabLayoutTest {
+    @Rule
+    @JvmField
+    val rule = ActivityScenarioRule(TabLayoutActivity::class.java)
+
+    @Test
+    fun testTabLayout() {
+        onScreen<TabLayoutActivityScreen> {
+            tabLayout {
+                isTabSelected(0)
+                assertEquals(0, getSelectedItem())
+
+                selectTab(1)
+
+                isTabSelected(1)
+                assertEquals(1, getSelectedItem())
+            }
+        }
+    }
+}
