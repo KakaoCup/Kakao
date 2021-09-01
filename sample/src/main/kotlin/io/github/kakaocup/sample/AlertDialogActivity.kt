d22dd6acf740d4b2f00baf6509da9510f0699d47
Konstantin Aksenov <aksenov.kostya@gmail.com>
2021-09-01 03:49:46 -0700

    Add options for AlertDialog

--- sample/src/main/kotlin/io/github/kakaocup/sample/AlertDialogActivity.kt
+++ sample/src/main/kotlin/io/github/kakaocup/sample/AlertDialogActivity.kt
@@ -11,12 +11,27 @@ class AlertDialogActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alert_dialog)
 
+        val singleChoiceItems = listOf("First", "Second", "Third")
+
         findViewById<View>(R.id.show_alert_dialog).setOnClickListener {
             AlertDialog
                 .Builder(this)
                 .setTitle("Title")
                 .setIcon(android.R.drawable.arrow_up_float)
                 .setMessage("Message")
+                .setPositiveButton("Positive") { _, _ -> }
+                .setNegativeButton("Negative") { _, _ -> }
+                .setNeutralButton("Neutral") { _, _ -> }
+                .create()
+                .show()
+        }
+
+        findViewById<View>(R.id.show_multi_choice_dialog).setOnClickListener {
+            AlertDialog
+                .Builder(this)
+                .setTitle("Title")
+                .setIcon(android.R.drawable.arrow_up_float)
+                .setSingleChoiceItems(singleChoiceItems.toTypedArray(), 0) { _, _ -> }
                 .setPositiveButton("Positive") { _, _ -> }
                 .setNegativeButton("Negative") { _, _ -> }
                 .setNeutralButton("Neutral") { _, _ -> }
