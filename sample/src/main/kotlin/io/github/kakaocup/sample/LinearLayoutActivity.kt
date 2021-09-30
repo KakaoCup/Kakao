f1ad5775e0f46bf8c8ce44999aaf63b7fa6cc0f6
Konstantin Aksenov <aksenov.kostya@gmail.com>
2021-09-29 19:37:38 -0700

    add-linear-layout-example

---
+++ sample/src/main/kotlin/io/github/kakaocup/sample/LinearLayoutActivity.kt
@@ -0,0 +1,14 @@
+package io.github.kakaocup.sample
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
