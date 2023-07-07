4aee98ef26a710042367be5f2ab9832724503206
Dmitriy Voronin <dmitriy.voronin@revolut.com>
2023-07-07 10:59:07 +0200

    ClickAction now accepts CoordinatesProvider
    add VisibleCenterGlobalCoordinatesProvider for special cases with translation animations involved

--- kakao-ext-clicks/src/main/kotlin/io/github/kakaocup/kakao/ext/clicks/KakaoDoubleClick.kt
+++ kakao-ext-clicks/src/main/kotlin/io/github/kakaocup/kakao/ext/clicks/KakaoDoubleClick.kt
@@ -1,6 +1,6 @@
 package io.github.kakaocup.kakao.ext.clicks
 
-import androidx.test.espresso.action.GeneralLocation
+import androidx.test.espresso.action.CoordinatesProvider
 import androidx.test.espresso.action.Press
 import io.github.kakaocup.kakao.common.actions.clicks.ClickAction
 import io.github.kakaocup.kakao.ext.clicks.inprocess.DoubleClickEvent
@@ -8,7 +8,7 @@ import io.github.kakaocup.kakao.ext.clicks.inprocess.InProcessClickAction
 import io.github.kakaocup.kakao.ext.clicks.visualization.VisualClicksConfig
 
 class KakaoDoubleClick(private val visualClicksConfig: VisualClicksConfig? = null) : ClickAction {
-    override fun click(location: GeneralLocation) = InProcessClickAction(
+    override fun click(location: CoordinatesProvider) = InProcessClickAction(
         clickEvent = DoubleClickEvent(visualClicksConfig),
         coordinatesProvider = location,
         precisionDescriber = Press.FINGER,
--- kakao/src/main/kotlin/io/github/kakaocup/kakao/common/actions/clicks/EspressoDoubleClick.kt
+++ kakao/src/main/kotlin/io/github/kakaocup/kakao/common/actions/clicks/EspressoDoubleClick.kt
@@ -2,13 +2,13 @@ package io.github.kakaocup.kakao.common.actions.clicks
 
 import android.view.InputDevice
 import android.view.MotionEvent
+import androidx.test.espresso.action.CoordinatesProvider
 import androidx.test.espresso.action.GeneralClickAction
-import androidx.test.espresso.action.GeneralLocation
 import androidx.test.espresso.action.Press
 import androidx.test.espresso.action.Tap
 
 class EspressoDoubleClick : ClickAction {
-    override fun click(location: GeneralLocation) = GeneralClickAction(
+    override fun click(location: CoordinatesProvider) = GeneralClickAction(
         Tap.DOUBLE, location, Press.FINGER,
         InputDevice.SOURCE_UNKNOWN, MotionEvent.BUTTON_PRIMARY
     )
