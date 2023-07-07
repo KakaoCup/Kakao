c53305f06eb3ea960a5e83c64d9282f81428e882
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-07-07 11:43:24 +1000

    feat(clicks): convert click coordinates from view to onScreen

--- sample/src/androidTest/kotlin/io/github/kakaocup/sample/PickersTest.kt
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/PickersTest.kt
@@ -4,6 +4,8 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import io.github.kakaocup.kakao.screen.Screen
 import io.github.kakaocup.sample.screen.PickersActivityScreen
+import io.github.kakaocup.sample.tools.applyEspressoClickExtension
+import io.github.kakaocup.sample.tools.applyKakaoClickExtension
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -89,4 +91,85 @@ class PickersTest {
             }
         }
     }
+
+    @Test
+    fun testDatePickerDialogKakaoClicks() {
+        applyKakaoClickExtension()
+        Screen.onScreen<PickersActivityScreen> {
+            selectDateButton {
+                click()
+            }
+
+            datePickerDialog {
+                datePicker {
+                    setDate(1955, 11, 12)
+                    hasDate(1955, 11, 12)
+                }
+                cancelButton {
+                    click()
+                }
+            }
+
+            selectDateButton {
+                click()
+            }
+
+            datePickerDialog {
+                datePicker {
+                    setDate(1955, 11, 12)
+                }
+                okButton {
+                    click()
+                }
+            }
+
+            dateText {
+                hasText("12 11 1955")
+            }
+        }
+        applyEspressoClickExtension()
+    }
+
+    @Test
+    fun testTimePickerDialogKakaoClicks() {
+        applyKakaoClickExtension()
+        Screen.onScreen<PickersActivityScreen> {
+            selectTimeButton {
+                click()
+            }
+
+            timePickerDialog {
+                timePicker {
+                    setTime(22, 4)
+                    hasTime(22, 4)
+                }
+                cancelButton {
+                    click()
+                }
+            }
+
+            selectTimeButton {
+                click()
+            }
+
+            timePickerDialog {
+                timePicker {
+                    setTime(22, 4)
+                }
+                okButton {
+                    click()
+                }
+            }
+
+            timeText {
+                hasText("22:4")
+            }
+
+            selectDateButton {
+                click()
+            }
+        }
+        applyEspressoClickExtension()
+    }
 }
+
--- sample/src/androidTest/kotlin/io/github/kakaocup/sample/SpinnerTest.kt
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/SpinnerTest.kt
@@ -5,6 +5,7 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import io.github.kakaocup.kakao.screen.Screen.Companion.onScreen
 import io.github.kakaocup.kakao.spinner.KSpinnerItem
 import io.github.kakaocup.sample.screen.SpinnerActivityScreen
+import io.github.kakaocup.sample.tools.applyEspressoClickExtension
 import io.github.kakaocup.sample.tools.applyKakaoClickExtension
 import org.junit.Rule
 import org.junit.Test
@@ -54,5 +55,47 @@ class SpinnerTest {
                 hasText("Title 4")
             }
         }
+    }
+
+    @Test
+    fun testContentItemsSpinnerKakaoClicks() {
+        applyKakaoClickExtension()
+        onScreen<SpinnerActivityScreen> {
+            spinner {
+                isVisible()
+                hasSize(10)
+
+                open()
+
+                emptyFirstChild {
+                    isVisible()
+                    hasText("Title 0")
+                }
+
+                childAt<KSpinnerItem>(1) {
+                    isVisible()
+                    hasText("Title 1")
+                }
+
+                emptyLastChild {
+                    isVisible()
+                    hasText("Title 9")
+                }
+
+                emptyChildWith {
+                    isInstanceOf(String::class.java)
+                    equals("Title 5")
+                }
+
+                emptyChildAt(4) {
+                    isDisplayed()
+                    hasText("Title 4")
+                    click()
+                }
+
+                hasText("Title 4")
+            }
+        }
+        applyEspressoClickExtension()
     }
 }
