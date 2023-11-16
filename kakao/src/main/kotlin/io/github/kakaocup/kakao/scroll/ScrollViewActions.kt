4f54d8cb3d7cc3c59f730ce8aea6f4cfd18d4c2b
Nikita Evdokimov <nikita-e57@yandex.ru>
2023-11-16 15:22:26 +0300

    Make ScrollViewActions support horizontal and nested scroll views

--- kakao/src/main/kotlin/io/github/kakaocup/kakao/scroll/ScrollViewActions.kt
+++ kakao/src/main/kotlin/io/github/kakaocup/kakao/scroll/ScrollViewActions.kt
@@ -3,13 +3,17 @@
 package io.github.kakaocup.kakao.scroll
 
 import android.view.View
+import android.widget.HorizontalScrollView
 import android.widget.ScrollView
+import androidx.core.widget.NestedScrollView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import io.github.kakaocup.kakao.common.actions.ScrollableActions
 import io.github.kakaocup.kakao.common.actions.SwipeableActions
 import org.hamcrest.Matchers
+import org.hamcrest.Matchers.anyOf
 
 /**
  * Provides ScrollableActions implementation for ScrollView
@@ -24,11 +28,20 @@ interface ScrollViewActions : ScrollableActions, SwipeableActions {
             override fun getDescription() = "Scroll ScrollView to start"
 
             override fun getConstraints() =
-                Matchers.allOf(ViewMatchers.isAssignableFrom(ScrollView::class.java), ViewMatchers.isDisplayed())
+                Matchers.allOf(
+                    anyOf(
+                        isAssignableFrom(ScrollView::class.java),
+                        isAssignableFrom(NestedScrollView::class.java),
+                        isAssignableFrom(HorizontalScrollView::class.java),
+                    ),
+                    ViewMatchers.isDisplayed()
+                )
 
             override fun perform(uiController: UiController?, view: View?) {
-                if (view is ScrollView) {
-                    view.fullScroll(View.FOCUS_UP)
+                when (view) {
+                    is ScrollView -> view.fullScroll(View.FOCUS_UP)
+                    is NestedScrollView -> view.fullScroll(View.FOCUS_UP)
+                    is HorizontalScrollView -> view.fullScroll(View.FOCUS_LEFT)
                 }
             }
         })
@@ -39,11 +52,20 @@ interface ScrollViewActions : ScrollableActions, SwipeableActions {
             override fun getDescription() = "Scroll ScrollView to end"
 
             override fun getConstraints() =
-                Matchers.allOf(ViewMatchers.isAssignableFrom(ScrollView::class.java), ViewMatchers.isDisplayed())
+                Matchers.allOf(
+                    anyOf(
+                        isAssignableFrom(ScrollView::class.java),
+                        isAssignableFrom(NestedScrollView::class.java),
+                        isAssignableFrom(HorizontalScrollView::class.java),
+                    ),
+                    ViewMatchers.isDisplayed()
+                )
 
             override fun perform(uiController: UiController?, view: View?) {
-                if (view is ScrollView) {
-                    view.fullScroll(View.FOCUS_DOWN)
+                when (view) {
+                    is ScrollView -> view.fullScroll(View.FOCUS_DOWN)
+                    is NestedScrollView -> view.fullScroll(View.FOCUS_DOWN)
+                    is HorizontalScrollView -> view.fullScroll(View.FOCUS_RIGHT)
                 }
             }
         })
@@ -54,11 +76,20 @@ interface ScrollViewActions : ScrollableActions, SwipeableActions {
             override fun getDescription() = "Scroll ScrollView to $position Y position"
 
             override fun getConstraints() =
-                Matchers.allOf(ViewMatchers.isAssignableFrom(ScrollView::class.java), ViewMatchers.isDisplayed())
+                Matchers.allOf(
+                    anyOf(
+                        isAssignableFrom(ScrollView::class.java),
+                        isAssignableFrom(NestedScrollView::class.java),
+                        isAssignableFrom(HorizontalScrollView::class.java),
+                    ),
+                    ViewMatchers.isDisplayed()
+                )
 
             override fun perform(uiController: UiController?, view: View?) {
-                if (view is ScrollView) {
-                    view.scrollTo(0, position)
+                when (view) {
+                    is ScrollView -> view.scrollTo(0, position)
+                    is NestedScrollView -> view.scrollTo(0, position)
+                    is HorizontalScrollView -> view.scrollTo(position, 0)
                 }
             }
         })
