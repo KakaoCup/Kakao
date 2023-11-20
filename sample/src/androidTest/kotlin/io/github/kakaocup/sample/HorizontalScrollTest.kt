1d4b4b789346e686d5d2a8ec6e0c7ccc20038324
Nikita Evdokimov <nikita-e57@yandex.ru>
2023-11-20 14:16:09 +0300

    Add tests

---
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/HorizontalScrollTest.kt
@@ -0,0 +1,33 @@
+package io.github.kakaocup.sample
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import io.github.kakaocup.kakao.screen.Screen.Companion.onScreen
+import io.github.kakaocup.sample.screen.ScrollScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HorizontalScrollTest {
+    @Rule
+    @JvmField
+    val rule = ActivityScenarioRule(HorizontalScrollActivity::class.java)
+
+    @Test
+    fun test() {
+        onScreen<ScrollScreen> {
+            checkStart()
+            scroll { scrollToEnd() }
+            checkEnd()
+
+            scroll { scrollToStart() }
+            checkStart()
+
+            scroll { scrollTo(5000) }
+            checkEnd()
+        }
+    }
+}
+
