8998b45224d22f49e0b799cd4cdebaf3f32b9c25
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-02-06 10:37:22 +1000

    fix(core): remove toolchain

--- buildSrc/src/main/kotlin/convention.kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/convention.kotlin.gradle.kts
@@ -3,9 +3,3 @@ import io.github.kakaocup.withVersionCatalog
 plugins {
     kotlin("android")
 }
-
-withVersionCatalog { libs ->
-    kotlin {
-        jvmToolchain(libs.versions.jvmVersion.get().toInt())
-    }
-}
