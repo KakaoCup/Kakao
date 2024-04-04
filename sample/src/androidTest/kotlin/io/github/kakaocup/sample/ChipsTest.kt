bade9df3bbe651ce80cc20e61790af58c6406ce2
Bartłomiej Twaróg <b.twarog90@gmail.com>
2024-04-04 10:33:52 +0200

    Add example test for new activity

---
+++ sample/src/androidTest/kotlin/io/github/kakaocup/sample/ChipsTest.kt
@@ -0,0 +1,38 @@
+package io.github.kakaocup.sample
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import io.github.kakaocup.kakao.screen.Screen.Companion.onScreen
+import io.github.kakaocup.sample.screen.ChipsScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ChipsTest {
+    @Rule
+    @JvmField
+    val rule = ActivityScenarioRule(ChipsActivity::class.java)
+
+    @Test
+    fun testContentItemsRecyclerView() {
+        onScreen<ChipsScreen> {
+            chip1 {
+                hasText("Chip1")
+                hasCheckedIcon(R.drawable.ic_sentiment_very_satisfied_black_24dp)
+            }
+            chip2 {
+                hasText("Chip2")
+                hasChipIcon(R.drawable.ic_auto_fix_high_24dp)
+            }
+            chip3 {
+                hasText("Chip3")
+                hasCloseIcon(R.drawable.ic_android_black_24dp, tintColorId = android.R.color.black)
+            }
+            chip4 {
+                hasText("Chip4")
+                hasNoIconVisible()
+            }
+        }
+    }
+}
